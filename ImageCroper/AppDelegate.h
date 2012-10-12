bf1e1b53e1350b55eb16436d09bd6100550ef726
gpdi <gpdi@gpdi.local>
2012-10-12 14:42:02 +0800

    Initial Commit

---
+++ ImageCroper/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ImageCroper
+//
+//  Created by hongming qin on 12-10-12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ImageCroper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ImageCroper
+//
+//  Created by hongming qin on 12-10-12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
